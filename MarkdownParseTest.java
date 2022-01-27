0644cc4bbd84c13ce6bde584388398305ec15239
noahdj37 <noahdj37@gmail.com>
2022-01-27 15:27:59 -0800

    Added test files
    added test files from lab description

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -17,10 +17,34 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void contents() throws IOException{
+    public void testFileContents() throws IOException{
 		Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals(List.of("https://something.com", "some-page.html"), links);
+    }
+
+    @Test
+    public void test2Contents() throws IOException{
+		Path fileName = Path.of("test2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://duckduckgo.com", "https://kde.org"), links);
+    }
+
+    @Test
+    public void test3Contents() throws IOException{
+		Path fileName = Path.of("test3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of(), links);
+    }
+
+    @Test
+    public void test4Contents() throws IOException{
+		Path fileName = Path.of("test4.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of(), links);
     }
 }
