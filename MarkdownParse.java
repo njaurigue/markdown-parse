f649692bd80701ee8c51440706610880791de89f
noahdj37 <noahdj37@gmail.com>
2022-01-27 11:31:47 -0800

    New tests
    Added tests, fixed MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,14 +12,20 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(nextOpenBracket == -1) {
+                break;
+            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(markdown.substring(openParen + 1, closeParen).indexOf(" ") == -1){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
+                nextCloseBracket + 1 != openParen ||
+                markdown.substring(openParen + 1, closeParen).indexOf(" ") != -1) {
+                    currentIndex = closeParen + 1;
+                    continue;
             }
-
-            currentIndex = closeParen + 1;
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;          
         }
         return toReturn;
     }
--- breaking-test-file.md
+++
@@ -1,4 +0,0 @@
-[a link!](https://something.com)
-[another link!](some-page.html)
-![an image](image.jpg)
-[third link](this is a sentence.)
