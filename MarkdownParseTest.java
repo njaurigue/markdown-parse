53f54addc53797ce1fd95efb8c56a78307622c44
noahdj37 <noahdj37@gmail.com>
2022-02-03 12:05:48 -0800

    Update MarkdownParseTest.java
    fixed tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -15,18 +15,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testFileOne() throws IOException {
-        assertEquals(List.of("Invalid Link", "some-page.html"),
+        assertEquals(List.of("https://something.com", "some-page.html"),
                 MarkdownParse.getLinks(Files.readString(Path.of("./test-file.md"))));
-    }
-
-    @Test(expected = IndexOutOfBoundsException.class)
-    public void testFileTwo() throws IOException {
-        MarkdownParse.getLinks(Files.readString(Path.of("./error-test-file.md")));
-    }
-
-    @Test
-    public void testFileThree() throws IOException {
-        assertEquals(List.of("https://www.minecraft.net/en-us", "image.png"),
-                MarkdownParse.getLinks(Files.readString(Path.of("./test-file-two.md"))));
     }
 }
