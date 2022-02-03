b154dbad30a22c44c444b83a1094e67cf66132de
noahdj37 <noahdj37@gmail.com>
2022-02-03 09:59:58 -0800

    Update MarkdownParse.java
    updated markdownparse to most recent successful form

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,20 +12,20 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket == -1) {
-                break;
-            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
-                nextCloseBracket + 1 != openParen ||
-                markdown.substring(openParen + 1, closeParen).indexOf(" ") != -1) {
-                    currentIndex = closeParen + 1;
-                    continue;
-            }
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+		        break;
+	        }
+	        if((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
+	                nextCloseBracket + 1 != openParen ||
+	                markdown.substring(openParen + 1, closeParen).indexOf(" ") != -1) {
+		        currentIndex = closeParen + 1;
+		        continue;
+	        }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;          
+            currentIndex = closeParen + 1;
         }
         return toReturn;
     }
