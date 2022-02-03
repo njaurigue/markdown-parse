08cf9cfcc76f3e0ec9ad860cbaf91f34d7966519
noahdj37 <noahdj37@gmail.com>
2022-02-03 11:54:41 -0800

    Update MarkdownParseTest.java
    new group tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,50 +1,32 @@
 import static org.junit.Assert.*;
-
-import java.io.IOException;
-
 import org.junit.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MarkdownParseTest {
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
 
     @Test
-    public void testFileContents() throws IOException{
-		Path fileName = Path.of("test-file.md");
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-        assertEquals(List.of("https://something.com", "some-page.html"), links);
+    public void testFileOne() throws IOException {
+        assertEquals(List.of("Invalid Link", "some-page.html"),
+                MarkdownParse.getLinks(Files.readString(Path.of("./test-file.md"))));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testFileTwo() throws IOException {
+        MarkdownParse.getLinks(Files.readString(Path.of("./error-test-file.md")));
     }
 
     @Test
-    public void test2Contents() throws IOException{
-		Path fileName = Path.of("test2.md");
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-        assertEquals(List.of("https://duckduckgo.com", "https://kde.org"), links);
-    }
-
-    @Test
-    public void test3Contents() throws IOException{
-		Path fileName = Path.of("test3.md");
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-        assertEquals(List.of(), links);
-    }
-
-    @Test
-    public void test4Contents() throws IOException{
-		Path fileName = Path.of("test4.md");
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-        assertEquals(List.of(), links);
+    public void testFileThree() throws IOException {
+        assertEquals(List.of("https://www.minecraft.net/en-us", "image.png"),
+                MarkdownParse.getLinks(Files.readString(Path.of("./test-file-two.md"))));
     }
 }
