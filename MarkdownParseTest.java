70573a7a58869ce2823f261ee7f44393819977ed
noahdj37 <noahdj37@gmail.com>
2022-02-22 15:41:47 -0800

    Create snippet files + tests
    Created files containing snippets to test, and 3 test methods for my implementation of markdown parse

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,11 +9,6 @@ import java.util.List;
 public class MarkdownParseTest {
 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-
-    @Test
     public void testFileOne() throws IOException {
         assertEquals(List.of("https://something.com", "some-page.html"),
                 MarkdownParse.getLinks(Files.readString(Path.of("./test-file.md"))));
@@ -22,5 +17,24 @@ public class MarkdownParseTest {
     @Test
     public void testFileThree() throws IOException {
         assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("./test3.md"))));
+    }
+
+    //Lab Report 4 - Week 8 Tests:
+    @Test
+    public void testSnippet1() throws IOException {
+        assertEquals(List.of("`google.com", "google.com", "ucsd.edu"),
+                MarkdownParse.getLinks(Files.readString(Path.of("./snippet1.md"))));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        assertEquals(List.of("a.com", "a.com(())", "example.com"),
+                MarkdownParse.getLinks(Files.readString(Path.of("./snippet2.md"))));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        assertEquals(List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/)", "https://cse.ucsd.edu/"),
+                MarkdownParse.getLinks(Files.readString(Path.of("./snippet3.md)"))));
     }
 }
